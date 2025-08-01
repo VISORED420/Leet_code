b65ab1f05b9a115e206c6971d9e0512cd59dc9ce
vaibhav mathpal <vaibhavmathpal@gmail.com>
2025-08-02 00:05:33 +0530

    Time: 39 ms (77.86%), Space: 45.5 MB (56.51%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> prefixSumFreq;
+        prefixSumFreq[0] = 1;
+        int sum = 0, count = 0;
+
+        for (int num : nums) {
+            sum += num;
+            if (prefixSumFreq.find(sum - k) != prefixSumFreq.end()) {
+                count += prefixSumFreq[sum - k];
+            }
+            prefixSumFreq[sum]++;
+        }
+
+        return count;
+    }
+};
